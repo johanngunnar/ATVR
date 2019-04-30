3f0ceb59e2cda2beb747c6aec047b3c1d4fa0090
Jóhann Gunnar <johanngunnar8@gmail.com>
2019-04-30 15:21:26 +0000

    ALAG_

--- ALAG/Create_alagsstudull.py
+++ ALAG/Create_alagsstudull.py
@@ -2,8 +2,11 @@ import csv
 from Alagsstudull import Create_alag
 from Update_alagsstudull import update_function_alagsstudull
 
-f1 = open('Voruspjald_Voruhus_afrit_ItemCAT_NEW.csv')
+#--------------------------------------------------
+#RUN THIS FILE TO UPDATE THE ALAG IN THE DATA BASE.
+#--------------------------------------------------
 
+f1 = open('Voruspjald_Voruhus_afrit_ItemCAT_NEW.csv')
 Item_Cat = []
 
 dreader = csv.DictReader(f1, delimiter=';')
@@ -12,28 +15,17 @@ for row in dreader:
 
 f1.close()
 
-
+#--------------------------------------------------
+#Load into dictonary key=number , value= tegund
+#--------------------------------------------------
 
 Item_cat_numbersDict = {}
-
-#Load into dictonary key=number , value= tegund
-
 for x in range(0,len(Item_Cat)):
 		Item_cat_numbersDict[int(float(Item_Cat[x]['\ufeffItem Category Code']))] = [Item_Cat[x]['Tegund'],Item_Cat[x]['Value']]
 
-#print key with value
-
-
-#for i in Item_cat_numbersDict:
-alex = 0.0
-print(Create_alag(Item_cat_numbersDict[1][0]))
-print(alex)
-
-print('hey')
-print(Item_cat_numbersDict[1][0])
-
-print(Item_cat_numbersDict)
-
+#--------------------------------------------------
+#USE ALAGSSTUDULL.PY TO CREATE ALAG FOR EACH PRODUCT
+#--------------------------------------------------
 
 for i in Item_cat_numbersDict:
 	Item_cat_numbersDict[i][1] = round(Create_alag(Item_cat_numbersDict[i][0]), 2)
@@ -43,4 +35,7 @@ for i in Item_cat_numbersDict:
 for i in sorted(Item_cat_numbersDict):
 	print(i, Item_cat_numbersDict[i])
 
+#--------------------------------------------------
+#NOW GO TO THE DATABASE AND UPDATE
+#--------------------------------------------------
 update_function_alagsstudull(Item_cat_numbersDict)
