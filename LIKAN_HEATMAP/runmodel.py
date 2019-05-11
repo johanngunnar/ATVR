2c43989decf5fe87360af0adc2ee703a051d6e5a
Hannes <hannesr17@ru.is>
2019-05-11 12:44:47 +0000

    Clean

--- LIKAN_HEATMAP/runmodel.py
+++ LIKAN_HEATMAP/runmodel.py
@@ -2,8 +2,7 @@
 
 f = open("runModelAndVisual.command","w+")
 
-for i in range(1,52):
-	f.write("glpsol --math -m bestun_likan_seventh_draft.mod -d demo_data_real{}.txt --check --wlp lp_output{}.lp\n".format(i,i))
-	f.write("gurobi_cl TimeLimit=10 ResultFile=solution{}.sol lp_output{}.lp\n".format(i,i))
-	f.write("\n")
+f.write("glpsol --math -m bestun_likan_seventh_draft.mod -d demo_data_real.txt --check --wlp lp_output.lp\n")
+f.write("gurobi_cl TimeLimit=120 ResultFile=solution.sol lp_output.lp")
+f.write("\n")
 f.write("python3 Visual_REALDATA_v4.py")
