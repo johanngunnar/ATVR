30fae356b637013fd084ef1f6c0799a07d6ddfb5
Hannes <hannesr17@ru.is>
2019-05-11 13:30:09 +0000

    Create nýja

--- LIKAN_HEATMAP/Actual/Create_realData_Actual.py
+++ LIKAN_HEATMAP/Actual/Create_realData_Actual.py
@@ -26,7 +26,7 @@ cursor = conn.cursor()
 # Write the select Q
 #----------------------------------------------------------------------------
 
-selectstring = Select_string(i)
+selectstring = Select_string(1)
 
 cursor.execute(selectstring)
 arr = cursor.fetchall()
@@ -188,30 +188,13 @@ f.write("set Bannlisti := \r\n")
 f.write(";\r\n")
 
 #s,t,d
-#print(arr)
 Start_day = int(arr[0][2][:2])
 f.write("set Fixlisti := \r\n")
 
-haesta_tala = arr[0][2][:2]
-heasta_man = arr[0][2][3:5]
 
 for i in range(1,Sendingar+1):
-	#print("I am failing at the indexes: ", i,arr[i][2][:2], "and", )
-	#print(i,arr[i][2][:2])
-	#day = int(arr[i][2][:2].strip())-Start_day+1
-	if int(haesta_tala.strip()) < int(arr[i][2][:2].strip()) and int(heasta_man.strip()) == int(arr[i][2][3:5].strip()):
-		haesta_tala = arr[i][2][:2]
-		heasta_man = arr[i][2][3:5]
-		day_count = day_count + 1
-	elif int(heasta_man.strip()) < int(arr[i][2][3:5].strip()):
-		haesta_tala = arr[i][2][:2]
-		heasta_man = arr[i][2][3:5]
-		day_count = day_count + 1
-	#print(arr[i-1][0:3])
-	#print(arr[i][0:3])
-	#print(day_count)
-	#print(haesta_tala)
-	#print(heasta_man)
+	print(i,arr[i][2][:2])
+	day = int(arr[i][2][:2].strip())-Start_day+1
 	slot_vendor_data = []
 	if arr[i][6].strip() == '420369-7789':
 		slot = 1
@@ -234,7 +217,7 @@ for i in range(1,Sendingar+1):
 	else:
 		slot = 8
 
-	f.write("{} {} {} \r\n".format(i,slot,day_count))
+	f.write("{} {} {} \r\n".format(i,slot,day))
 
 f.write(";\r\n")
 
